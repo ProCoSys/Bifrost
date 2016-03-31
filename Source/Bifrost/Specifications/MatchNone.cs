0a5f071a6bb3f0dc80fb9483a1d2df2a52a11ac5
Børge Nordli <bnordli@gmail.com>
2016-03-31 12:57:26 +0200

    Create MatchAll and MatchNone specifications.

---
+++ Source/Bifrost/Specifications/MatchNone.cs
@@ -0,0 +1,16 @@
+﻿namespace Bifrost.Specifications
+{
+    /// <summary>
+    /// Represents a <see cref="Specification{T}">rule</see> that matches no values.
+    /// </summary>
+    public class MatchNone<T> : Specification<T>
+    {
+        /// <summary>
+        /// Initializes an instance of <see cref="MatchNone{T}"/>.
+        /// </summary>
+        public MatchNone()
+        {
+            Predicate = a => false;
+        }
+    }
+}
