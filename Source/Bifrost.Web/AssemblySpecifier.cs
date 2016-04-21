ProCoSys/Bifrost

commit e079cb7059fa31422e657eeba2e154265fc344b1

Use Assembly opt-in by default. Breaking change! #5

Source/Bifrost.Web/AssemblySpecifier.cs
@@ -1,41 +1,0 @@
-﻿#region License
-//
-// Copyright (c) 2008-2015, Dolittle (http://www.dolittle.com)
-//
-// Licensed under the MIT License (http://opensource.org/licenses/MIT)
-//
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the license at
-//
-//   http://github.com/dolittle/Bifrost/blob/master/MIT-LICENSE.txt
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-#endregion
-using Bifrost.Configuration.Assemblies;
-using Bifrost.Execution;
-
-namespace Bifrost.Web
-{
-    /// <summary>
-    /// Reperesents an <see cref="ICanSpecifyAssemblies">assembly specifier</see> for client aspects
-    /// </summary>
-    public class AssemblySpecifier : ICanSpecifyAssemblies
-    {
-#pragma warning disable 1591 // Xml Comments
-        public void Specify(IAssemblyRuleBuilder builder)
-        {
-            builder.ExcludeAssembliesStartingWith(
-                "Antlr3",
-                "Owin",
-                "WebActivator",
-                "WebGrease"
-            );
-        }
-#pragma warning disable 1591 // Xml Comments
-    }
-}

Source/Bifrost.Web/Bifrost.Web.csproj
@@ -99,7 +99,6 @@
     <Compile Include="Applications\ApplicationRoute.cs" />
     <Compile Include="Applications\ApplicationRouteHandler.cs" />
     <Compile Include="Applications\ApplicationRouteHttpHandler.cs" />
-    <Compile Include="AssemblySpecifier.cs" />
     <Compile Include="Assets\AssetManagerRoute.cs" />
     <Compile Include="Assets\AssetManagerRouteHandler.cs" />
     <Compile Include="Assets\AssetManagerRouteHttpHandler.cs" />

Source/Bifrost/Configuration/Configure.cs
@@ -287,7 +287,7 @@ namespace Bifrost.Configuration
         {
             var builder = new AssembliesConfigurationBuilder();
             if (assembliesConfigurationBuilderCallback != null) assembliesConfigurationBuilderCallback(builder);
-            if (builder.RuleBuilder == null) builder.IncludeAll();
+            if (builder.RuleBuilder == null) builder.IncludeNone();
             return builder;
         }
 

Source/Bifrost.Autofac/AssemblySpecifier.cs
@@ -1,38 +1,0 @@
-﻿#region License
-//
-// Copyright (c) 2008-2015, Dolittle (http://www.dolittle.com)
-//
-// Licensed under the MIT License (http://opensource.org/licenses/MIT)
-//
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the license at
-//
-//   http://github.com/dolittle/Bifrost/blob/master/MIT-LICENSE.txt
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-#endregion
-using Bifrost.Configuration.Assemblies;
-using Bifrost.Execution;
-
-namespace Bifrost.Autofac
-{
-    /// <summary>
-    /// Reperesents an <see cref="ICanSpecifyAssemblies">assembly specifier</see> for client aspects
-    /// </summary>
-    public class AssemblySpecifier : ICanSpecifyAssemblies
-    {
-#pragma warning disable 1591 // Xml Comments
-        public void Specify(IAssemblyRuleBuilder builder)
-        {
-            builder.ExcludeAssembliesStartingWith(
-                "Autofac"
-            );
-        }
-#pragma warning disable 1591 // Xml Comments
-    }
-}

Source/Bifrost.Client/AssemblySpecifier.cs
@@ -1,42 +1,0 @@
-﻿#region License
-//
-// Copyright (c) 2008-2015, Dolittle (http://www.dolittle.com)
-//
-// Licensed under the MIT License (http://opensource.org/licenses/MIT)
-//
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the license at
-//
-//   http://github.com/dolittle/Bifrost/blob/master/MIT-LICENSE.txt
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-#endregion
-using Bifrost.Configuration.Assemblies;
-using Bifrost.Execution;
-
-namespace Bifrost
-{
-    /// <summary>
-    /// Reperesents an <see cref="ICanSpecifyAssemblies">assembly specifier</see> for client aspects
-    /// </summary>
-    public class AssemblySpecifier : ICanSpecifyAssemblies
-    {
-#pragma warning disable 1591 // Xml Comments
-        public void Specify(IAssemblyRuleBuilder builder)
-        {
-            builder.ExcludeAssembliesStartingWith(
-                "Presentation",
-                "vshost",
-                "WindowsBase",
-                "UIAutomation",
-                "Castle.Core"
-            );
-        }
-#pragma warning disable 1591 // Xml Comments
-    }
-}

Source/Bifrost.EntityFramework/AssemblySpecifier.cs
@@ -1,38 +1,0 @@
-﻿#region License
-//
-// Copyright (c) 2008-2015, Dolittle (http://www.dolittle.com)
-//
-// Licensed under the MIT License (http://opensource.org/licenses/MIT)
-//
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the license at
-//
-//   http://github.com/dolittle/Bifrost/blob/master/MIT-LICENSE.txt
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-#endregion
-using Bifrost.Configuration.Assemblies;
-using Bifrost.Execution;
-
-namespace Bifrost.EntityFramework
-{
-    /// <summary>
-    /// Reperesents an <see cref="ICanSpecifyAssemblies">assembly specifier</see> for client aspects
-    /// </summary>
-    public class AssemblySpecifier : ICanSpecifyAssemblies
-    {
-#pragma warning disable 1591 // Xml Comments
-        public void Specify(IAssemblyRuleBuilder builder)
-        {
-            builder.ExcludeAssembliesStartingWith(
-                "EntityFramework"
-            );
-        }
-#pragma warning disable 1591 // Xml Comments
-    }
-}

Source/Bifrost.FluentValidation/AssemblySpecifier.cs
@@ -1,38 +1,0 @@
-﻿#region License
-//
-// Copyright (c) 2008-2015, Dolittle (http://www.dolittle.com)
-//
-// Licensed under the MIT License (http://opensource.org/licenses/MIT)
-//
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the license at
-//
-//   http://github.com/dolittle/Bifrost/blob/master/MIT-LICENSE.txt
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-#endregion
-using Bifrost.Configuration.Assemblies;
-using Bifrost.Execution;
-
-namespace Bifrost.FluentValidation
-{
-    /// <summary>
-    /// Reperesents an <see cref="ICanSpecifyAssemblies">assembly specifier</see> for client aspects
-    /// </summary>
-    public class AssemblySpecifier : ICanSpecifyAssemblies
-    {
-#pragma warning disable 1591 // Xml Comments
-        public void Specify(IAssemblyRuleBuilder builder)
-        {
-            builder.ExcludeAssembliesStartingWith(
-                "FluentValidation"
-            );
-        }
-#pragma warning disable 1591 // Xml Comments
-    }
-}

Source/Bifrost.JSON/AssemblySpecifier.cs
@@ -1,38 +1,0 @@
-﻿#region License
-//
-// Copyright (c) 2008-2015, Dolittle (http://www.dolittle.com)
-//
-// Licensed under the MIT License (http://opensource.org/licenses/MIT)
-//
-// You may not use this file except in compliance with the License.
-// You may obtain a copy of the license at
-//
-//   http://github.com/dolittle/Bifrost/blob/master/MIT-LICENSE.txt
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-#endregion
-using Bifrost.Configuration.Assemblies;
-using Bifrost.Execution;
-
-namespace Bifrost.JSON
-{
-    /// <summary>
-    /// Reperesents an <see cref="ICanSpecifyAssemblies">assembly specifier</see> for client aspects
-    /// </summary>
-    public class AssemblySpecifier : ICanSpecifyAssemblies
-    {
-#pragma warning disable 1591 // Xml Comments
-        public void Specify(IAssemblyRuleBuilder builder)
-        {
-            builder.ExcludeAssembliesStartingWith(
-                "Newtonsoft.Json"
-            );
-        }
-#pragma warning disable 1591 // Xml Comments
-    }
-}

commit c504e9e8b736d6618df90fcfdf655a8b536e4746

Make sample projects run again. (Regression from e079cb70, #5 and 7ac379b3, #31.)

Source/Bifrost.Web/AssemblySpecifier.cs
@@ -1,0 +1,13 @@
+using Bifrost.Configuration.Assemblies;
+using Bifrost.Execution;
+
+namespace Bifrost.Web
+{
+    public class AssemblySpecifier : ICanSpecifyAssemblies
+    {
+        public void Specify(IAssembliesConfiguration configuration)
+        {
+            configuration.IncludeAssembliesStartingWith("Web");
+        }
+    }
+}

Source/Bifrost.Web/Bifrost.Web.csproj
@@ -105,6 +105,7 @@
     <Compile Include="Applications\RouteExtensions.cs" />
     <Compile Include="Assets\AssetsManager.cs" />
     <Compile Include="Assets\IAssetsManager.cs" />
+    <Compile Include="AssemblySpecifier.cs" />
     <Compile Include="Commands\CommandContextConnectionManager.cs" />
     <Compile Include="Commands\CommandCoordinator.cs" />
     <Compile Include="Commands\CommandCoordinatorService.cs" />

Source/Bifrost/Configuration/Configure.cs
@@ -90,6 +90,7 @@ namespace Bifrost.Configuration
             var canCreateContainerInstance = instanceCreator.Create<ICanCreateContainer>();
 
             var container = canCreateContainerInstance.CreateContainer();
+            container.Bind<ICanCreateContainer>(canCreateContainerInstance);
 
             InitializeDefaults(container, assembliesConfiguration, assemblyProvider, contractToImplementorsMap);
 