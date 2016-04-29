ProCoSys/Bifrost

commit c01d3485bdee454dff7ccd74271531dd7d9204ac

Take default conventions and default bindings out of Configure constructor and Configure.With. #31

Source/Bifrost/Configuration/Configure.cs
@@ -46,19 +46,12 @@ namespace Bifrost.Configuration
         /// </summary>
         public static Assembly EntryAssembly => Instance.Container.Get<ICanCreateContainer>().GetType().Assembly;
 
-        Configure(
-            IContainer container,
-            IDefaultConventions defaultConventions,
-            IDefaultBindings defaultBindings)
+        Configure(IContainer container)
         {
             SystemName = "[Not Set]";
             container.Bind<IConfigure>(this);
 
             Container = container;
-
-            defaultBindings.Initialize(Container);
-            defaultConventions.Initialize();
-
             InitializeProperties();
         }
 
@@ -97,34 +90,48 @@ namespace Bifrost.Configuration
             var canCreateContainerInstance = instanceCreator.Create<ICanCreateContainer>();
 
             var container = canCreateContainerInstance.CreateContainer();
-            var configure = With(
-                container,
+
+            InitializeDefaults(container, assembliesConfiguration, assemblyProvider, contractToImplementorsMap);
+
+            var configure = With(container);
+            configure.Initialize();
+            return configure;
+        }
+
+        /// <summary>
+        /// Initialize default bindings and conventions from supplied parameters.
+        /// </summary>
+        public static void InitializeDefaults(
+            IContainer container,
+            IAssembliesConfiguration assembliesConfiguration,
+            IAssemblyProvider assemblyProvider,
+            IContractToImplementorsMap contractToImplementorsMap)
+        {
+            var defaultBindings = new DefaultBindings(
                 assembliesConfiguration,
                 assemblyProvider,
                 contractToImplementorsMap);
-            configure.Initialize();
-            return configure;
+            var defaultConventions = new DefaultConventions(container);
+            defaultBindings.Initialize(container);
+            defaultConventions.Initialize();
         }
 
         /// <summary>
         /// Configure with a specific <see cref="IContainer"/>.
         /// </summary>
         /// <param name="container"><see cref="IContainer"/> to configure with.</param>
-        /// <param name="assembliesConfiguration"><see cref="IAssembliesConfiguration"/> to use.</param>
-        /// <param name="assemblyProvider"><see cref="IAssemblyProvider"/> to use for providing assemblies.</param>
-        /// <param name="contractToImplementorsMap"><see cref="IContractToImplementorsMap"/> for keeping track of
-        /// the relationship between contracts and implementors.</param>
         /// <returns>Configuration object to continue configuration on.</returns>
-        public static Configure With(
-            IContainer container,
-            IAssembliesConfiguration assembliesConfiguration,
-            IAssemblyProvider assemblyProvider,
-            IContractToImplementorsMap contractToImplementorsMap)
+        public static Configure With(IContainer container)
         {
-            return With(
-                container,
-                new DefaultConventions(container),
-                new DefaultBindings(assembliesConfiguration, assemblyProvider, contractToImplementorsMap));
+            if (Instance == null)
+            {
+                lock (InstanceLock)
+                {
+                    Instance = new Configure(container);
+                }
+            }
+
+            return Instance;
         }
 
         /// <summary>
@@ -133,30 +140,6 @@ namespace Bifrost.Configuration
         public static void Reset()
         {
             lock (InstanceLock) Instance = null;
-        }
-
-        /// <summary>
-        /// Configure with a specific <see cref="IContainer"/>, <see cref="IDefaultConventions"/>
-        /// and <see cref="IDefaultBindings"/>.
-        /// </summary>
-        /// <param name="container"><see cref="IContainer"/> to configure with</param>
-        /// <param name="defaultConventions"><see cref="IDefaultConventions"/> to use</param>
-        /// <param name="defaultBindings"><see cref="IDefaultBindings"/> to use</param>
-        /// <returns></returns>
-        public static Configure With(
-            IContainer container,
-            IDefaultConventions defaultConventions,
-            IDefaultBindings defaultBindings)
-        {
-            if (Instance == null)
-            {
-                lock (InstanceLock)
-                {
-                    Instance = new Configure(container, defaultConventions, defaultBindings);
-                }
-            }
-
-            return Instance;
         }
 
 #pragma warning disable 1591 // Xml Comments

Source/Bifrost.Specs/Configuration/for_Configure/given/a_configure_instance.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using Bifrost.Configuration;
-using Bifrost.Configuration.Assemblies;
-using Bifrost.Configuration.Defaults;
 using Bifrost.Execution;
 using Machine.Specifications;
 using Moq;
@@ -13,8 +11,6 @@ namespace Bifrost.Specs.Configuration.for_Configure.given
         protected static Configure configure_instance;
         protected static Mock<IContainer> container_mock;
         protected static Mock<ICommandsConfiguration> commands_configuration_mock;
-        protected static Mock<IDefaultConventions> default_conventions_mock;
-        protected static Mock<IDefaultBindings> default_bindings_mock;
         protected static Mock<IEventsConfiguration> events_configuration_mock;
         protected static Mock<ITasksConfiguration> tasks_configuration_mock;
         protected static Mock<ISagasConfiguration> sagas_configuration_mock;
@@ -31,57 +27,66 @@ namespace Bifrost.Specs.Configuration.for_Configure.given
 
 
         Establish context = () =>
-                                {
-                                    Configure.Reset();
-                                    container_mock = new Mock<IContainer>();
-                                    default_conventions_mock = new Mock<IDefaultConventions>();
-                                    default_bindings_mock = new Mock<IDefaultBindings>();
+        {
+            Configure.Reset();
+            container_mock = new Mock<IContainer>();
 
-                                    commands_configuration_mock = new Mock<ICommandsConfiguration>();
-                                    container_mock.Setup(c => c.Get<ICommandsConfiguration>()).Returns(commands_configuration_mock.Object);
+            commands_configuration_mock = new Mock<ICommandsConfiguration>();
+            container_mock.Setup(c => c.Get<ICommandsConfiguration>()).Returns(commands_configuration_mock.Object);
 
-                                    events_configuration_mock = new Mock<IEventsConfiguration>();
-                                    container_mock.Setup(c => c.Get<IEventsConfiguration>()).Returns(events_configuration_mock.Object);
+            events_configuration_mock = new Mock<IEventsConfiguration>();
+            container_mock.Setup(c => c.Get<IEventsConfiguration>()).Returns(events_configuration_mock.Object);
 
-                                    tasks_configuration_mock = new Mock<ITasksConfiguration>();
-                                    container_mock.Setup(c => c.Get<ITasksConfiguration>()).Returns(tasks_configuration_mock.Object);
+            tasks_configuration_mock = new Mock<ITasksConfiguration>();
+            container_mock.Setup(c => c.Get<ITasksConfiguration>()).Returns(tasks_configuration_mock.Object);
 
-                                    views_configuration_mock = new Mock<IViewsConfiguration>();
-                                    container_mock.Setup(c => c.Get<IViewsConfiguration>()).Returns(views_configuration_mock.Object);
+            views_configuration_mock = new Mock<IViewsConfiguration>();
+            container_mock.Setup(c => c.Get<IViewsConfiguration>()).Returns(views_configuration_mock.Object);
 
-                                    sagas_configuration_mock = new Mock<ISagasConfiguration>();
-                                    container_mock.Setup(c => c.Get<ISagasConfiguration>()).Returns(sagas_configuration_mock.Object);
+            sagas_configuration_mock = new Mock<ISagasConfiguration>();
+            container_mock.Setup(c => c.Get<ISagasConfiguration>()).Returns(sagas_configuration_mock.Object);
 
-                                    serialization_configuration_mock = new Mock<ISerializationConfiguration>();
-                                    container_mock.Setup(c => c.Get<ISerializationConfiguration>()).Returns(serialization_configuration_mock.Object);
+            serialization_configuration_mock = new Mock<ISerializationConfiguration>();
+            container_mock.Setup(c => c.Get<ISerializationConfiguration>())
+                .Returns(serialization_configuration_mock.Object);
 
-                                    default_storage_configuration_mock = new Mock<IDefaultStorageConfiguration>();
-                                    container_mock.Setup(c => c.Get<IDefaultStorageConfiguration>()).Returns(default_storage_configuration_mock.Object);
+            default_storage_configuration_mock = new Mock<IDefaultStorageConfiguration>();
+            container_mock
+                .Setup(c => c.Get<IDefaultStorageConfiguration>())
+                .Returns(default_storage_configuration_mock.Object);
 
-                                    frontend_configuration_mock = new Mock<IFrontendConfiguration>();
-                                    container_mock.Setup(c => c.Get<IFrontendConfiguration>()).Returns(frontend_configuration_mock.Object);
+            frontend_configuration_mock = new Mock<IFrontendConfiguration>();
+            container_mock.Setup(c => c.Get<IFrontendConfiguration>()).Returns(frontend_configuration_mock.Object);
 
-                                    call_context_configuration_mock = new Mock<ICallContextConfiguration>();
-                                    container_mock.Setup(c => c.Get<ICallContextConfiguration>()).Returns(call_context_configuration_mock.Object);
+            call_context_configuration_mock = new Mock<ICallContextConfiguration>();
+            container_mock
+                .Setup(c => c.Get<ICallContextConfiguration>())
+                .Returns(call_context_configuration_mock.Object);
 
-                                    execution_context_configuration_mock = new Mock<IExecutionContextConfiguration>();
-                                    container_mock.Setup(c => c.Get<IExecutionContextConfiguration>()).Returns(execution_context_configuration_mock.Object);
+            execution_context_configuration_mock = new Mock<IExecutionContextConfiguration>();
+            container_mock
+                .Setup(c => c.Get<IExecutionContextConfiguration>())
+                .Returns(execution_context_configuration_mock.Object);
 
-                                    security_configuration_mock = new Mock<ISecurityConfiguration>();
-                                    container_mock.Setup(c => c.Get<ISecurityConfiguration>()).Returns(security_configuration_mock.Object);
+            security_configuration_mock = new Mock<ISecurityConfiguration>();
+            container_mock.Setup(c => c.Get<ISecurityConfiguration>()).Returns(security_configuration_mock.Object);
 
-                                    type_importer_mock = new Mock<ITypeImporter>();
-                                    container_mock.Setup(c => c.Get<ITypeImporter>()).Returns(type_importer_mock.Object);
+            type_importer_mock = new Mock<ITypeImporter>();
+            container_mock.Setup(c => c.Get<ITypeImporter>()).Returns(type_importer_mock.Object);
 
-                                    configure_instance = Configure.With(container_mock.Object, default_conventions_mock.Object, default_bindings_mock.Object);
+            configure_instance = Configure.With(container_mock.Object);
 
-                                    configurators_mock = new Mock<IInstancesOf<ICanConfigure>>();
-                                    configurators_mock.Setup(c => c.GetEnumerator()).Returns(new List<ICanConfigure>().GetEnumerator());
-                                    container_mock.Setup(c => c.Get<IInstancesOf<ICanConfigure>>()).Returns(configurators_mock.Object);
+            configurators_mock = new Mock<IInstancesOf<ICanConfigure>>();
+            configurators_mock.Setup(c => c.GetEnumerator()).Returns(new List<ICanConfigure>().GetEnumerator());
+            container_mock.Setup(c => c.Get<IInstancesOf<ICanConfigure>>()).Returns(configurators_mock.Object);
 
-                                    after_configuration_callbacks_mock = new Mock<IInstancesOf<IWantToKnowWhenConfigurationIsDone>>();
-                                    after_configuration_callbacks_mock.Setup(a => a.GetEnumerator()).Returns(new List<IWantToKnowWhenConfigurationIsDone>().GetEnumerator());
-                                    container_mock.Setup(c => c.Get<IInstancesOf<IWantToKnowWhenConfigurationIsDone>>()).Returns(after_configuration_callbacks_mock.Object);
-                                };
+            after_configuration_callbacks_mock = new Mock<IInstancesOf<IWantToKnowWhenConfigurationIsDone>>();
+            after_configuration_callbacks_mock
+                .Setup(a => a.GetEnumerator())
+                .Returns(new List<IWantToKnowWhenConfigurationIsDone>().GetEnumerator());
+            container_mock
+                .Setup(c => c.Get<IInstancesOf<IWantToKnowWhenConfigurationIsDone>>())
+                .Returns(after_configuration_callbacks_mock.Object);
+        };
     }
 }

commit fb3abe486b677f3edf290acf18866e34c58fa6e7

Don't register IContainer twice. (Regression from c01d3485bd.)

Source/Bifrost/Configuration/Configure.cs
@@ -31,6 +31,7 @@ namespace Bifrost.Configuration
     /// <summary>
     /// Represents the default <see cref="IConfigure"/> type.
     /// </summary>
+    [IgnoreDefaultConvention]
     public class Configure : IConfigure
     {
         static readonly object InstanceLock = new object();