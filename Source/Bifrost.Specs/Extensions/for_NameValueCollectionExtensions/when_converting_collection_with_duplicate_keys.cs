e937cb2a5f375bcaece594040d108b9f44f8ff0c
Børge Nordli <bnordli@gmail.com>
2016-06-16 09:59:32 +0200

    Create a NameValueCollectionExtension to convert from a NameValueCollection to Dictionary.

---
+++ Source/Bifrost.Specs/Extensions/for_NameValueCollectionExtensions/when_converting_collection_with_duplicate_keys.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using Bifrost.Extensions;
+using Machine.Specifications;
+
+namespace Bifrost.Specs.Extensions.for_NameValueCollectionExtensions
+{
+    [Subject(typeof(NameValueCollectionExtensions))]
+    public class when_converting_collection_with_duplicate_keys
+    {
+        static IDictionary<string, string> result;
+
+        Because of = () => result = new NameValueCollection { { "A", "3" }, { "A", "4" } }.ToDictionary();
+
+        It should_create_dictionary_with_one_value = () => result.Count.ShouldEqual(1);
+
+        It should_concatenate_values = () => result["A"].ShouldEqual("3,4");
+    }
+}
---
+++ Source/Bifrost/Extensions/NameValueCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿#region License
+//
+// Copyright (c) 2008-2015, Dolittle (http://www.dolittle.com)
+//
+// Licensed under the MIT License (http://opensource.org/licenses/MIT)
+//
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the license at
+//
+//   http://github.com/dolittle/Bifrost/blob/master/MIT-LICENSE.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Bifrost.Extensions
+{
+    /// <summary>
+    /// Provides a set of methods for working with <see cref="NameValueCollection">types</see>.
+    /// </summary>
+    public static class NameValueCollectionExtensions
+    {
+        /// <summary>
+        /// Converts a collection to a dictionary.
+        /// </summary>
+        /// <param name="collection">The collection to convert.</param>
+        /// <returns>A dictionary where the duplicate keys of the collection are converted to comma separated values.</returns>
+        public static IDictionary<string, string> ToDictionary(this NameValueCollection collection)
+        {
+            return collection.AllKeys.ToDictionary(k => k, k => collection[k]);
+        }
+    }
+}
