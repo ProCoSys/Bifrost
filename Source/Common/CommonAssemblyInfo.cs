4fa8aaac35301a2c90c8ad85178717d6154f6c38
Anders Nygaard <andeny@statoil.com>
2017-08-23 12:31:50 +0200

    Bumped version to 3.0.12

--- Source/Common/CommonAssemblyInfo.cs
+++ Source/Common/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2008-2015 Dolittle")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.0.11")]
-[assembly: AssemblyInformationalVersion("3.0.11")]
+[assembly: AssemblyVersion("3.0.12")]
+[assembly: AssemblyInformationalVersion("3.0.12")]
 
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
